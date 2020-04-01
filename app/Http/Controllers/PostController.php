96fe17030317a73d38e96de5b0631532cfcd0c71
aqshalzakki <kecoatempur2@gmail.com>
2020-04-01 16:35:17 +0700

    create a new index method for post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers;
 use App\Post;
 use App\User;
 use App\Http\Resources\Post as PostResource;
+use App\Http\Resources\PostCollection;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        return new PostCollection(Post::all());
+    }
+
     public function store()
     {
     	$data = request()->validate([
