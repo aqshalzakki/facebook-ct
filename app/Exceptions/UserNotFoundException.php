1a984099e20c3fd1109c2c71814160dd42ac6039
aqshalzakki <kecoatempur2@gmail.com>
2020-04-07 23:44:32 +0700

    Create UserNotFoundException.php

---
+++ app/Exceptions/UserNotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class UserNotFoundException extends Exception
+{
+    /**
+     * Render the exception as an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        return response()->json([
+            'errors' => [
+                'status' => 404,
+                'title' => 'User not Found!',
+                'detail' => 'Unable to locate the user with the given information.',
+            ]
+        ], 404);
+    }
+}
